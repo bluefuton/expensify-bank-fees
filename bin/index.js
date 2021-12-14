38da0eedec5668534c31afe25c3ed1b35cd1865a
Chris R <chris.rosser@automattic.com>
2021-12-14 15:40:48 +1300

    Fix output

--- bin/index.js
+++ bin/index.js
@@ -7,7 +7,7 @@ prompt.start();
 
 prompt.get(['report_title', 'expected_amount_nzd', 'received_amount_nzd'], function (err, result) {
     if (err) { return onErr(err); }
-    const outputText = `Bank fees for ${result.report_title} expenses. Expected $241.45, received around $219.45.`;
+    const outputText = `Bank fees for ${result.report_title} expenses. Expected ${result.expected_amount_nzd}, received around ${result.received_amount_nzd}.`;
     console.log( chalk.yellow.bold( outputText ) );
 });
 
