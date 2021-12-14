cba3842a076ad98f3ea2982645e98402755cb024
Chris R <chris.rosser@automattic.com>
2021-12-14 15:50:21 +1300

    Add amount to expense

--- bin/index.js
+++ bin/index.js
@@ -5,10 +5,11 @@ import chalk from 'chalk';
 
 prompt.start();
 
-prompt.get(['report_title', 'expected_amount_nzd', 'received_amount_nzd'], function (err, result) {
+prompt.get(['report_title', 'expected_amount_usd', 'received_amount_usd'], function (err, result) {
     if (err) { return onErr(err); }
-    const outputText = `Bank fees for ${result.report_title} expenses. Expected ${result.expected_amount_nzd}, received around ${result.received_amount_nzd}.`;
+    const outputText = `Bank fees for ${result.report_title} expenses. Expected $${result.expected_amount_usd}, received around $${result.received_amount_usd}.`;
     console.log( chalk.yellow.bold( outputText ) );
+    console.log( 'Amount to expense: $' + ( result.expected_amount_usd - result.received_amount_usd ).toFixed(2) + ' USD')
 });
 
 function onErr(err) {
